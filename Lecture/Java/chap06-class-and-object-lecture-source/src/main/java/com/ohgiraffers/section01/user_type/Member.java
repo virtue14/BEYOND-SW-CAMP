557766726ae4a4d6b2b5a32d79add2eb86a319dc
DemianStudio <rudejrqkr97@gmail.com>
2024-01-19 00:41:26 +0900

    :rocket: 53. 수업목표: 클래스가 무엇인지 이해하고 작성할 수 있다.
    + Application.java
    + Member.java
    📌 목차
    1. 변수를 이용한 회원 데이터 관리

---
+++ Lecture/Java/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section01/user_type/Member.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section01.user_type;
+/* 설명. 여러가지 타입의 값을 하나의 타입(Member Class)으로 묶을 수 있다.(C++의 구조체에 가까움) */
+public class Member {
+    String id;
+    String pwd;
+    String name;
+    int age;
+    char gender;
+    String[] hobby;
+}
