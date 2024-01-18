894a9359d97a55ad6e8a11f093219fc060642a70
DemianStudio <rudejrqkr97@gmail.com>
2024-01-19 00:50:12 +0900

    :rocket: 64. 수업목표: 초기화블럭의 동작 순서를 이해하고 이를 활용하여 인스턴스를 생성할 수 있다.
    + Application.java
    + Product.java

---
+++ Lecture/Java/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section07/initblock/Application.java
@@ -0,0 +1,12 @@
+/* 수업목표: 초기화블럭의 동작 순서를 이해하고 이를 활용하여 인스턴스를 생성할 수 있다. */
+package com.ohgiraffers.section07.initblock;
+
+public class Application {
+    public static void main(String[] args) {
+        Product product = new Product();
+        System.out.println(product);
+//        Product product2 = new Product("샤오미");
+
+    }
+}
+
