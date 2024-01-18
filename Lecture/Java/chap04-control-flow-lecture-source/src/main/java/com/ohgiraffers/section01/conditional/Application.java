2073dfa1b1e0fae8df59da597581d8ab744781a5
DemianStudio <rudejrqkr97@gmail.com>
2024-01-19 00:06:11 +0900

    :boom: section01 실행용 Application.java
    + Application.java

---
+++ Lecture/Java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/Application.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section01.conditional;
+
+public class Application {
+    public static void main(String[] args) {
+
+//        A_if aClass = new A_if();
+        /* 필기. 단독 if문 흐름 확인용 메소드 호출 */
+//        aClass.testSimpleIfStatement();
+
+        /* 필기. 중첩 if문 흐름 확인용 메소드 호출*/
+//        aClass.testNestedIfStatement();
+
+//        B_ifElse bClass = new B_ifElse();
+        /* 필기. 단독 if-else문 흐름 확인용 메소드 호출 */
+//        bClass.testSimpleIfElseStatement();
+        /* 필기. 중첩 if-else문 흐름 확인용 메소드 호충 */
+//        bClass.NestedIfElseStatement();
+
+//        C_ifElseIf cClass = new C_ifElseIf();
+        /* 필기. 단독 if-else-if문 흐름 확인용 메소드 호출 */
+//        cClass.testSimpleIfElseIfStatement();
+        /* 필기. 중첩 if-else-if문 흐름 확인용 메소드 호출 */
+//        cClass.testNestedIfElseIfStatement();
+
+        D_switch dClass = new D_switch();
+        /* 필기. switch문 흐름 확인용 메소드 호출 */
+        dClass.testSimpleSwitchStatement();
+
+    }
+}
