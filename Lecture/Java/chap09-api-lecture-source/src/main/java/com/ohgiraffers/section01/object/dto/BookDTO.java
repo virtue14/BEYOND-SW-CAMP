7a1cfc15ab608a50ebe2a5709d028c509cad210e
DemianStudio <rudejrqkr97@gmail.com>
2024-01-19 11:24:58 +0900

    :bug: 77. 수업목표: Object 클래스의 hashCode() 메소드 오버라이딩 목적을 이해하고 활용할 수 있다.
    + Application3.java
    + [modify] BookDTO.java

--- Lecture/Java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/dto/BookDTO.java
+++ Lecture/Java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/dto/BookDTO.java
@@ -75,5 +75,11 @@ public class BookDTO {
         return number == bookDTO.number && price == bookDTO.price && Objects.equals(title, bookDTO.title) && Objects.equals(author, bookDTO.author);
     }
 
+    /* 목차 3. hashCode() 오버라이딩 */
+    /* 설명: 우리가 정한 동등 기준을 만족하면 같은 값이 나오도록 오버라이딩 */
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, title, author, price);
+    }
 
 }
