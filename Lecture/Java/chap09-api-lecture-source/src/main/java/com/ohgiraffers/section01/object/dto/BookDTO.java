c709ce75129f61027b5e65ab0fbcb0308c2b98a5
DemianStudio <rudejrqkr97@gmail.com>
2024-01-19 11:17:13 +0900

    :bug: 75. 수업목표: Object 클래스의 toString() 메소드 오버라이딩 목적을 이해하고 활용할 수 있다.
    + Application1.java
    + BookDTO.java

---
+++ Lecture/Java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/dto/BookDTO.java
@@ -0,0 +1,64 @@
+package com.ohgiraffers.section01.object.dto;
+
+import java.util.Objects;
+
+public class BookDTO {
+
+    private int number;
+    private String title;
+    private String author;
+    private int price;
+
+    public BookDTO(int number, String title, String author, int price) {
+        this.number = number;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    /* 목차 1. toString() 오버라이딩 */
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+                "number=" + number +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+
+
+}
---
+++ Lecture/Java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/run/Application1.java
@@ -0,0 +1,27 @@
+/* 수업목표: Object 클래스의 toString() 메소드 오버라이딩 목적을 이해하고 활용할 수 있다. */
+package com.ohgiraffers.section01.object.run;
+
+import com.ohgiraffers.section01.object.dto.BookDTO;
+
+import java.util.Arrays;
+
+public class Application1 {
+    public static void main(String[] args) {
+
+        BookDTO book1 = new BookDTO(1, "홍길동전", "허균", 500000);
+        BookDTO book2 = new BookDTO(2, "목민심서", "정약용", 300000);
+        BookDTO book3 = new BookDTO(3, "목민심서", "정약용", 300000);
+
+        /* 설명: 각 인스턴스의 toString()을 호출하려면 각 인스턴스가 가진 필드 값을 문자열로 반환한다. */
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+        System.out.println(book3.toString());
+
+        System.out.println();
+
+        /* 설명: 참조 자료형 변수를 print 또는 println으로 출력하면 해당 객체의 toString()을 자동호출한다. */
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+    }
+}
