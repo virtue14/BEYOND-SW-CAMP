87f2eafc1a1684b3a5a314446429f49b3481c4d2
DemianStudio <rudejrqkr97@gmail.com>
2024-01-22 15:48:35 +0900

    :pencil2: 95. 수업목표: extends 키워드를 사용하여 특정 타입만 제네릭 타입으로 사용하도록 제한할 수 있다.
    + Application1.java
    + Animal.java
    + Mammal.java
    + Reptile.java
    + Rabbit.java
    + Bunny.java
    + DrunkenBunny.java
    + RabbitFarm.java

---
+++ Lecture/Java/chap10-generics-lecture-source/src/main/java/com/ohgiraffers/section02/extend/vo/DrunkenBunny.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section02.extend.vo;
+
+public class DrunkenBunny extends Rabbit{
+    @Override
+    public void cry() {
+        System.out.println("봐니봐니 봐니뽜니 당근! 당근!#@!@#?");
+    }
+}
