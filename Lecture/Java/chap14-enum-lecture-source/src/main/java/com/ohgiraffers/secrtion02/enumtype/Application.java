fc9daefa0d0d95938572f38a4e839c96e89223da
DemianStudio <rudejrqkr97@gmail.com>
2024-01-25 12:14:30 +0900

    :construction_worker: 124. 수업목표: 열거형(Enum)을 이용하여 상수 열거 패턴의 단점을 해결하는 것을 이해할 수 있다.
    + Application.java
    + Subjects.java

---
+++ Lecture/Java/chap14-enum-lecture-source/src/main/java/com/ohgiraffers/secrtion02/enumtype/Application.java
@@ -0,0 +1,44 @@
+/* 수업목표: 열거형(Enum)을 이용하여 상수 열거 패턴의 단점을 해결하는 것을 이해할 수 있다. */
+
+package com.ohgiraffers.secrtion02.enumtype;
+
+import com.ohgiraffers.secrtion02.enumtype.Subjects;
+
+public class Application {
+    public static void main(String[] args) {
+        /* 설명 1. 열거 타입으로 선언된 인스턴스는 싱글톤으로 관리되며 인스턴스가 한 개 임을 보장한다.(순서 개념의 인덱스가 아니라 인스턴스임) */
+        Subjects subjects1 = Subjects.JAVA;
+        Subjects subjects2 = Subjects.HTML;
+        Subjects subjects3 = Subjects.JAVA;
+
+        if (subjects1 == subjects2) {
+            System.out.println(" 두 과목은 같은 과목입니다.");
+        } else {
+            System.out.println("두 과목은 다른 과목입니다.");
+        }
+
+        if (subjects1 == subjects3) {
+            System.out.println("싱글톤이다.");
+        }
+
+        /* 설명 3. toString()/name()을 이용하여 필드명을 문자열로 변경하기 쉽다.(feat.switch 안씀) */
+        System.out.println(Subjects.JAVA.toString());
+        System.out.println(Subjects.JAVA.name());
+
+        /* 설명 4. values()를 이용하여 상수값 배열을 반환받고 이를 이용하여 연속처리할 수 있다. */
+        Subjects[] subjects = Subjects.values();
+        for (Subjects s : subjects) {
+            System.out.println(s.toString());
+            System.out.println(s.ordinal());
+            System.out.println(s.name());
+            System.out.println();
+        }
+
+        /* 설명 5. 타입 안전성을 보장할 수 있다. */
+        printSubjects(Subjects.JAVA);
+//        printSubjects(0);         // Subjects 타입이 아니라서 에러 발생
+    }
+
+    private static void printSubjects(Subjects subjects) {
+    }
+}
