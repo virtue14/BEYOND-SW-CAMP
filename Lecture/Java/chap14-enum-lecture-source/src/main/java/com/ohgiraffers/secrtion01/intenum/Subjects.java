eb6394f4239ea0c858171aab9afa4c22f480dd1c
DemianStudio <rudejrqkr97@gmail.com>
2024-01-25 12:14:10 +0900

    :construction_worker: 123. 수업목표: 정수 열거 패턴과 이의 단점을 이해할 수 있다.(열거형(enum) 아닐 때)
    + Application.java
    + Subjects.java

---
+++ Lecture/Java/chap14-enum-lecture-source/src/main/java/com/ohgiraffers/secrtion01/intenum/Subjects.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.secrtion01.intenum;
+
+public interface Subjects {
+    public static final int JAVA = 0;
+    public static final int MARIADB = 1;
+    public static final int JDBC = 2;
+
+    public static final int HTML = 0;
+    public static final int CSS = 1;
+    public static final int JAVASCRIPT = 2;
+}
