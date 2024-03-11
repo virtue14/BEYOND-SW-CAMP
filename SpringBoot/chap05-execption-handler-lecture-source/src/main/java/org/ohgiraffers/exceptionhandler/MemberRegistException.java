9d85d612a4ab48fa7d2cd313a87fc8af3b66d2e2
악덕 <rudejrqkr97@gmail.com>
2024-03-11 14:25:27 +0900

    :tada: [New] SpringBoot-Chap05-ExceptionHandler-Lecture-Source

---
+++ SpringBoot/chap05-execption-handler-lecture-source/src/main/java/org/ohgiraffers/exceptionhandler/MemberRegistException.java
@@ -0,0 +1,7 @@
+package org.ohgiraffers.exceptionhandler;
+
+public class MemberRegistException extends Exception {
+    public MemberRegistException(String message) {
+        super(message);
+    }
+}
---
+++ SpringBoot/chap05-execption-handler-lecture-source/src/test/java/org/ohgiraffers/exceptionhandler/Chap05ExecptionHandlerLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.exceptionhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap05ExecptionHandlerLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
