81b7ab5babb8e1e6a4f616d322fd0af2887632be
악덕 <rudejrqkr97@gmail.com>
2024-03-11 12:46:28 +0900

    :tada: [New] SpringBoot-Chap01-AutoConfiguration-Lecture-Source

---
+++ SpringBoot/chap01-auto-configuration-lecture-source/src/test/java/org/ohgiraffers/autoconfiguration/Chap01AutoConfigurationLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.autoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01AutoConfigurationLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
