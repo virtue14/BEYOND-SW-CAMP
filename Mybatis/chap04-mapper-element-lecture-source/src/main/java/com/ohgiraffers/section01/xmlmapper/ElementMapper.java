96d5c580341122d9add88026410bf54a939e44dc
악덕 <rudejrqkr97@gmail.com>
2024-03-11 15:22:26 +0900

    :tada: [New] Mybatis-Chap04-Element-Lecture-Source

---
+++ Mybatis/chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/ElementMapper.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import java.util.List;
+
+public interface ElementMapper {
+
+    List<MenuDTO> selectResultMapTest();
+
+    List<MenuANDCategoryDTO> selectResultMapAssociationTest();
+
+    List<CategoryAndMenuDTO> selectResultMapCollectionTest();
+}
