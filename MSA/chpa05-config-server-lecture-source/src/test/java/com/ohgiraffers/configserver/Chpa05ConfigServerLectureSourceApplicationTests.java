b1b44ded45812ef1052c4b6c3eeca3cbb552a5f1
악덕 <rudejrqkr97@gmail.com>
2024-03-11 16:47:01 +0900

    :tada: [New] MSA-Chap05-ConfigServer-Lecture-Source

---
+++ MSA/chpa05-config-server-lecture-source/src/test/java/com/ohgiraffers/configserver/Chpa05ConfigServerLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chpa05ConfigServerLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
