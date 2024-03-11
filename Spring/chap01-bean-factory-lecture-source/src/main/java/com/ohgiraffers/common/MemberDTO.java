9766ff99629d90dc835975409b7f0083c1bb5918
악덕 <rudejrqkr97@gmail.com>
2024-03-11 12:09:59 +0900

    :tada: [New] Spring-Chap01-Factory-Lecture-Source

---
+++ Spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/common/MemberDTO.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.common;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class MemberDTO {
+    private int sequence;
+    private String id;
+    private String pwd;
+    private String name;
+
+}
