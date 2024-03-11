f14cfed6ce7b36c3a2421905eb5a7f006847694a
악덕 <rudejrqkr97@gmail.com>
2024-03-11 10:54:27 +0900

    :memo: [Add] Section02 - PreparedStatement

---
+++ JDBC/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section02/prepared/Application1.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.section02.prepared;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.ohgiraffers.common.JDBCTemplate.close;
+import static com.ohgiraffers.common.JDBCTemplate.getConnection;
+
+public class Application1 {
+    public static void main(String[] args) {
+        Connection con = getConnection();
+
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+
+        try {
+            /* 설명: Statement와 달리 PreparedStatement는 생성 당시에 쿼리가 있어야 한다. */
+            pstmt = con.prepareStatement("SELECT EMP_ID, EMP_NAME FROM EMPLOYEE");
+            rset = pstmt.executeQuery();
+
+            while (rset.next()) {
+                System.out.println(rset.getString("EMP_ID") + ", " + rset.getString("EMP_NAME"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(rset);
+            close(pstmt);
+            close(con);
+        }
+    }
+}
