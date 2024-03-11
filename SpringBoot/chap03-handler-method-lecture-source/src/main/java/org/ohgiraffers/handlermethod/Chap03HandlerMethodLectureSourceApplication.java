dcdcddf0222879693c312821b4ab7621f5b3896b
악덕 <rudejrqkr97@gmail.com>
2024-03-11 14:19:55 +0900

    :tada: [New] SpringBoot-Chap03-HandlerMethod-Lecture-Source

---
+++ SpringBoot/chap03-handler-method-lecture-source/src/main/java/org/ohgiraffers/handlermethod/Chap03HandlerMethodLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.handlermethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap03HandlerMethodLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap03HandlerMethodLectureSourceApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/chap03-handler-method-lecture-source/src/test/java/org/ohgiraffers/handlermethod/Chap03HandlerMethodLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.handlermethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap03HandlerMethodLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
