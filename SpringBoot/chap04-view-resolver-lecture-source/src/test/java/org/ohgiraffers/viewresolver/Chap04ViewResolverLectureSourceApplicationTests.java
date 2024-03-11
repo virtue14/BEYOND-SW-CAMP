f008755c8f8a2bb141bac8c817eb225cee255b14
악덕 <rudejrqkr97@gmail.com>
2024-03-11 14:23:44 +0900

    :tada: [New] SpringBoot-Chap04-ViewResolver-Lecture-Source

---
+++ SpringBoot/chap04-view-resolver-lecture-source/src/test/java/org/ohgiraffers/viewresolver/Chap04ViewResolverLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.viewresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap04ViewResolverLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
