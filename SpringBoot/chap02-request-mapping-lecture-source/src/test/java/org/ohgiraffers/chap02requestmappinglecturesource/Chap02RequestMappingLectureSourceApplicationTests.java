a453e0c52911f3a0ae262a5388ef401ddf35e71f
악덕 <rudejrqkr97@gmail.com>
2024-03-11 14:10:54 +0900

    :tada: [New] SpringBoot-Chap02-RequestMapping-Lecture-Source

---
+++ SpringBoot/chap02-request-mapping-lecture-source/src/test/java/org/ohgiraffers/chap02requestmappinglecturesource/Chap02RequestMappingLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.chap02requestmappinglecturesource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap02RequestMappingLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
