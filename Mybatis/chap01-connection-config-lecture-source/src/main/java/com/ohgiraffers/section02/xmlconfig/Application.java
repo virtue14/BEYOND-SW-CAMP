fcf4a1fe7b8a30db85311f674e2524261bd966f5
악덕 <rudejrqkr97@gmail.com>
2024-03-11 15:11:04 +0900

    :memo: [Add] Section02 - XMLConfig

---
+++ Mybatis/chap01-connection-config-lecture-source/src/main/java/com/ohgiraffers/section02/xmlconfig/Application.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.section02.xmlconfig;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Application {
+    public static void main(String[] args) {
+
+        String resource = "mybatis-config.xml";
+
+        InputStream inputStream;
+
+        {
+            try {
+                inputStream = Resources.getResourceAsStream(resource);
+
+                SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+                SqlSession session = sqlSessionFactory.openSession(false);
+
+                java.util.Date date = session.selectOne("mapper.selectNow");
+                System.out.println("date = " + date);
+                session.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
