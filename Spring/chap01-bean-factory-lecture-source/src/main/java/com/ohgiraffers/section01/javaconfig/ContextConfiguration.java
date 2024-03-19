a965b8a9746cff07c173b4168f84983c4db0d97e
악덕 <rudejrqkr97@gmail.com>
2024-03-19 10:47:29 +0900

    ✨ Feat: Added Java configuration for bean factory
    - 빈 팩토리를 위한 자바 구성을 추가함.

--- Spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/ContextConfiguration.java
+++ Spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/ContextConfiguration.java
@@ -4,11 +4,11 @@ import com.ohgiraffers.common.MemberDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class ContextConfiguration {
-    @Bean(name = "member")
+    @Bean(name="member")
     public MemberDTO getMember() {
         return new MemberDTO(1, "user01", "pass01", "홍길동");
     }
+
 }
