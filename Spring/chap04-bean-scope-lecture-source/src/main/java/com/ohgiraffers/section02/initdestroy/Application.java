6c2f9416c41708bfe7aaddb5ec194dc283e2bdb9
악덕 <rudejrqkr97@gmail.com>
2024-03-19 11:34:22 +0900

    🙈 chore(chap03-di-annotation-lecture-source): .gitignore 설정 파일 추가 - .gitignore 설정 파일을 추가하여 필요한 설정들을 추가했습니다.

--- Spring/chap04-bean-scope-lecture-source/src/main/java/com/ohgiraffers/section02/initdestroy/Application.java
+++ Spring/chap04-bean-scope-lecture-source/src/main/java/com/ohgiraffers/section02/initdestroy/Application.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(ContextConfiguration.class);
 
         String[] beanNames = context.getBeanDefinitionNames();
         for (String beanName : beanNames) {
