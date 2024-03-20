9b01e9c47249239cb536dd26b3cdaa935c27be23
악덕 <rudejrqkr97@gmail.com>
2024-03-20 09:37:37 +0900

    feat: add static index.html and controller for request mapping
    - 정적 index.html 파일과 요청 매핑을 위한 컨트롤러를 추가했습니다.

--- SpringBoot/chap02-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/Chap02RequestMappingLectureSourceApplication.java
+++ SpringBoot/chap02-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/Chap02RequestMappingLectureSourceApplication.java
@@ -1,4 +1,4 @@
-package org.ohgiraffers.chap02requestmappinglecturesource;
+package com.ohgiraffers.requestmapping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
