4de531cd9cbd7607788fed366b74ac6c477cfae2
악덕 <rudejrqkr97@gmail.com>
2024-03-20 10:33:59 +0900

    refactor(application.yml): 서비스 라우팅을 개선하고 주석을 정리함
    - 서비스 라우팅 및 주석 정리
    - uri 및 filters 수정
    - user-service와 order-service 추가
    - 토큰 확인 및 필터링 로직 개선
    - AuthorizationHeaderFilter 클래스 리팩토링
    - 토큰 유효성 검사 로직 추가
    - 에러 처리 메서드 개선

--- MSA/chap03-spring-cloud-gateway-lecture-source/src/main/java/com/ohgiraffers/gateway/filter/AuthorizationHeaderFilter.java
+++ MSA/chap03-spring-cloud-gateway-lecture-source/src/main/java/com/ohgiraffers/gateway/filter/AuthorizationHeaderFilter.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.gateway.filter;
 
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
@@ -31,29 +32,30 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
     }
 
-    /**
-     *  토큰을 Authorization 키 값으로 가지고 오는지 판별, 그 토큰이 유효한지 판별
-     */
+    /* 설명. 토큰을 Authorization 키 값으로 가지고 오는지 판별, 그 토큰이 유효한지 판별 */
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            /* 설명: ServerHttpRequest는 HttpServletRequest와 달리 스프링의 Webflux 기술을 활용(비동기 통신)하기 위한 request */
+
+            /* 설명. ServerHttpRequest는 HttpServletRequest와 달리 스프링의 Webflux 기술을 활용(비동기 통신)하기 위한 request */
             ServerHttpRequest request = exchange.getRequest();
 
-            /* 설명: 토큰 들고 오는지 확인(RequestHeader에 "Authorization"이라는 키 값이 넘어오나 */
-            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+            /* 설명. 토큰 들고 오는지 확인(RequestHeader에 "Authorization"이라는 키 값이 넘어오느냐) */
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                 return onError(exchange, "Authorization 헤더 부재", HttpStatus.UNAUTHORIZED);
             }
+
             HttpHeaders headers = request.getHeaders();
             Set<String> keys = headers.keySet();
             Iterator<String> iter = keys.iterator();
-            while (iter.hasNext()) {
+            while(iter.hasNext()) {
                 log.info(iter.next());
             }
 
             String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
             String jwt = authorizationHeader.replace("Bearer", "");
-            if (!isJwtValid(jwt)) {
+
+            if(!isJwtValid(jwt)) {
                 return onError(exchange, "토큰이 유효하지 않음", HttpStatus.UNAUTHORIZED);
             }
 
@@ -61,12 +63,12 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         };
     }
 
-    /**
-     *  유효한 토큰인지 확인 후 true 또는 false를 반환
-     */
+    /* 설명. 유효한 토큰인지 확인 후 true 또는 false를 반환 */
     private boolean isJwtValid(String jwt) {
         boolean flag = true;
+
         String subject = null;
+
         try {
             subject = Jwts
                     .parser()
@@ -77,18 +79,21 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
             e.printStackTrace();
             flag = false;
         }
-        if (subject == null || subject.isEmpty()) {
+
+        if(subject == null || subject.isEmpty()) {
             flag = false;
         }
+
         return flag;
     }
 
-    /**
-     *  에러 발생 시 401번 응답 코드로 반환
-     */
-    private Mono<Void> onError(ServerWebExchange exchange, String authorization_헤더_부재, HttpStatus httpStatus) {
+    /* 설명. 에러 발생 시 401번 응답 코드로 반환 */
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(httpStatus);
+
         return response.setComplete();
     }
+
+
 }
