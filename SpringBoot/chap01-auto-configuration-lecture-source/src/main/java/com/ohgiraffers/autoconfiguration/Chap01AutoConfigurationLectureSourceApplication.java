64bc4fadb77451aacc979db0d5fa1f1a825f08ef
악덕 <rudejrqkr97@gmail.com>
2024-03-20 09:35:29 +0900

    feat(chap01-auto-configuration-lecture-source): 새로운 테스트 및 애플리케이션 소스 추가
    - Chap01AutoConfigurationLectureSourceApplicationTests.java 및 Chap01AutoConfigurationLectureSourceApplication.java 파일 추가됨.

--- SpringBoot/chap01-auto-configuration-lecture-source/src/main/java/com/ohgiraffers/autoconfiguration/Chap01AutoConfigurationLectureSourceApplication.java
+++ SpringBoot/chap01-auto-configuration-lecture-source/src/main/java/com/ohgiraffers/autoconfiguration/Chap01AutoConfigurationLectureSourceApplication.java
@@ -1,4 +1,4 @@
-package org.ohgiraffers.autoconfiguration;
+package com.ohgiraffers.autoconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
