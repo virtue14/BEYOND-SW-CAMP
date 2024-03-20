2468dbf5bb26200162b0323f6463612c04d87093
악덕 <rudejrqkr97@gmail.com>
2024-03-20 10:30:22 +0900

    fix(chap02-01-first-service-lecture-source): Gateway 라우트 및 환경설정 값 관련 수정
    - Gateway 추가 후 Routes 경로 수정
    - Gateway에서 RewritePath 필터로 segment만 전달
    - application.yml 값 불러오기 위한 코드 추가
    - health_check 메서드와 message 메서드 수정
    - application.yml의 defaultZone URL 수정

--- MSA/chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
+++ MSA/chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
@@ -10,14 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-/* 설명: Gateway 추가 후 Routes 경로 추가 후 작성했던 부분 */
+
+/* 설명. Gateway 추가 후 Routes 경로 추가 후 작성했던 부분 */
 //@RequestMapping("/first-service")
 
-/* 설명: Gateway에서 RewritePath 필터 추가 후 뒤에 segment만 넘어오게 한 이후 */
+/* 설명. Gateway에서 RewritePath 필터 추가 후 뒤에 segment만 넘어오게 한 이후 */
 @RequestMapping("/")
 public class FirstServiceController {
 
-    /* 설명: @Value와 같이 application.yml 또는 환경설정 값 불러오기 위해 추가(생성자 주입) */
+    /* 설명. @Value와 같이 application.yml 또는 환경설정 값 불러오기 위해 추가(생성자 주입) */
     private Environment env;
 
     @Autowired
@@ -27,12 +28,13 @@ public class FirstServiceController {
 
     @GetMapping("health_check")
     public String healthCheck() {
-        /* 설명: first-service application 여러개 실행 시(스케일 아웃) 실행되는 어플리케이션 확인(포트 번호로) */
-        /* 설명: Gateway의 lib(로드 밸런싱)을 통해 RoundRobin 방식으로 스위칭 됨을 확인 */
-        return "I'm OK at" + env.getProperty("local.server.port");
+        
+        /* 설명. first-service 어플리케이션 여러개 실행 시(스케일 아웃) 실행되는 어플리케이션 확인(포트 번호로) */
+        /* 설명. Gateway의 lb(로드 밸런싱)를 통해 RoundRobin 방식으로 스위칭 됨을 확인 */
+        return "I'm OK port at " + env.getProperty("local.server.port");
     }
 
-    /* 설명: Gateway를 거치고 넘어오는 요청 시 RequestHeader에 값이 추가 됨(Gateway의 필터에 의해) */
+    /* 설명. Gateway를 거치고 넘어오는 요청 시 RequestHeader에 값이 추가 됨(Gateway의 필터에 의해) */
     @GetMapping("message")
     public String message(@RequestHeader("first-request") String header) {
         log.info("넘어온 헤더값: {}", header);
