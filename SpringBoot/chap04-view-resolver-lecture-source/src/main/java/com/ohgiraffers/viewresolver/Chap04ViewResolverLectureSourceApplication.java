93093e22bc9372785aeef6c5b81ac42a4d9dd1f0
악덕 <rudejrqkr97@gmail.com>
2024-03-20 09:39:35 +0900

    feat: 뷰 리졸버를 이용한 뷰 연결하기(feat.포워딩vs리다이렉트)
    - 서블릿 때와 마찬가지로 포워딩을 통해 값을 전달할 수 있다.
    - 서블릿 때와 마찬가지로 파라미터를 활용하지 않고서는 리다이렉트를 통해 값을 전달할 수 없다.(스프링은 해법이 있다.)
    - 스프링의 RedirectAttributes라는 객체에 attr을 담으면 리다이렉트 시에도 값(전달할 상태)이 유지 된다.
    - 기존에 핸들러 메소드가 'void 또는 String'으로만 반환했는

--- SpringBoot/chap04-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/Chap04ViewResolverLectureSourceApplication.java
+++ SpringBoot/chap04-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/Chap04ViewResolverLectureSourceApplication.java
@@ -1,4 +1,4 @@
-package org.ohgiraffers.viewresolver;
+package com.ohgiraffers.viewresolver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
