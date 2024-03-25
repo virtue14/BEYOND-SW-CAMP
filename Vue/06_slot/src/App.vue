6a0b643b30b3ca4ed23b5111773738417e25b3ab
악덕 <rudejrqkr97@gmail.com>
2024-03-25 14:05:41 +0900

    init

---
+++ Vue/06_slot/src/App.vue
@@ -0,0 +1,23 @@
+<!--
+  컴포넌트의 lifeCycle hooks를 사용하는 방식은 2가지가 있으며,
+  아래의 정리된 방법과 <script setup>으로 script를 setup 지시문으로 추가하여
+  hooks를 바로 사용할 수 있다.
+-->
+<script setup>
+  import {onMounted} from "vue";
+  export default {
+    setup() {
+      onMounted(async() => {
+        console.log("Hello! Vue~~!");
+      })
+    }
+  }
+</script>
+
+<template>
+  <p>Hello!</p>
+</template>
+
+<style scoped>
+
+</style>
