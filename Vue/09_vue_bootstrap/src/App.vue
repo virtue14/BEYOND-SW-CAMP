697f8bccad89fff55e3a7396b48c9d0c799dd7e6
악덕 <rudejrqkr97@gmail.com>
2024-03-26 15:53:27 +0900

    feat(App.vue): Vue 프로젝트에 부트스트랩 배지 추가
    - 부트스트랩 배지를 템플릿에 추가

--- Vue/09_vue_bootstrap/src/App.vue
+++ Vue/09_vue_bootstrap/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-
+    <b-badge href="#" variant="primary">Primary</b-badge>
+    <b-badge href="#" variant="secondary">Secondary</b-badge>
+    <b-badge href="#" variant="success">Success</b-badge>
+    <b-badge href="#" variant="danger">Danger</b-badge>
+    <b-badge href="#" variant="warning">Warning</b-badge>
+    <b-badge href="#" variant="info">Info</b-badge>
+    <b-badge href="#" variant="light">Light</b-badge>
+    <b-badge href="#" variant="dark">Dark</b-badge>
   </div>
 </template>
 
