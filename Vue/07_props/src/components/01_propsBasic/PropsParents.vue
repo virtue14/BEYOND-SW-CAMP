ea9e10820a14e4ac1680f1b1fd2f668b53c6b6e8
악덕 <rudejrqkr97@gmail.com>
2024-03-26 10:39:15 +0900

    feat(Props): PropsParents.vue에 ReadProps 컴포넌트 추가
    - PropsParents.vue에 ReadProps.vue를 추가했습니다.
    - readValue라는 데이터를 추가하고 초기값을 설정했습니다.
    - readValue를 자식 컴포넌트로 전달하고, 부모에서 조작할 수 있도록 했습니다.

--- Vue/07_props/src/components/01_propsBasic/PropsParents.vue
+++ Vue/07_props/src/components/01_propsBasic/PropsParents.vue
@@ -2,9 +2,11 @@
 import {ref} from "vue";
 import PropsChild from "@/components/01_propsBasic/PropsChild.vue";
 import DarkMode from "@/components/01_propsBasic/DarkMode.vue";
+import ReadProps from "@/components/01_propsBasic/ReadProps.vue";
 
 const message = ref('hello');
 const darkState = ref(false);
+const readValue = ref('vue는 재밌다.');
 
 function toggleDarkMode() {
   darkState.value = !darkState.value;
@@ -16,8 +18,20 @@ function toggleDarkMode() {
     props 입력: <input v-model="message"/>
     <PropsChild :message="message"/>
     <DarkMode @toggle="toggleDarkMode"/>
+    <ReadProps :readValue="readValue"/>
+    부모의 readValue: <input v-model="readValue"/>
   </div>
 </template>
+
+<!--
+    props:
+    부모에서 자식 컴포넌트로 데이터를 전달하는 경우 우리는 다양한 <slot>을 이용할 수 있다.
+    하지만 slot의 경우 컨텐츠(또는 구조)를 전달하기 위해 사용하는 것으로 단일 데이터를 전달하는 경우 적합하지 않으며, 부모에게 전달받은 데이터를
+    조작하는데도 어려움이 있다.
+    이러한 경우 우리는 데이터만 전달하기 위한 용도로 props를 사용할 수 있다.
+    (props는 자식에게 물려주면 자식 컴포넌트는 readonly(읽기전용)로 써야한다. vue는 자식 컴포넌트가 물려받은 값을
+    수정해도 어차피 부모 컴포넌트에게 영향을 주지 못해서 readonly 효과가 난다.)
+-->
 
 <style scoped>
 .container {
