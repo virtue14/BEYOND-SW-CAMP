6134f49b74272ed1e40938b47f7b8eb1c28e3408
악덕 <rudejrqkr97@gmail.com>
2024-03-26 09:46:29 +0900

    feat(01_propsBasic): 부모 컴포넌트에 다크 모드 토글 기능 추가
    - 'PropsParents.vue' 컴포넌트에 다크 모드 토글 함수와 상태를 추가
    - 'DarkMode.vue' 컴포넌트에 다크 모드 토글 버튼 및 이벤트 발생 함수 추가
    - 'PropsChild.vue' 컴포넌트에 부모로부터 받은 props를 출력하는 기능 추가

---
+++ Vue/07_props/src/components/01_propsBasic/DarkMode.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import {defineEmits} from 'vue';
+
+  /* 부모로 부터 물려받은 toggle이라는 이름의 이벤트를 호출할 수 있는 함수 반환받기(defineEmits를 활용하면 배열로 이벤트 이름 작성) */
+  const emit = defineEmits(['toggle']);
+</script>
+
+<template>
+  <button @click='emit("toggle")'>다크모드</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Vue/07_props/src/components/01_propsBasic/PropsParents.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from "vue";
+import PropsChild from "@/components/01_propsBasic/PropsChild.vue";
+import DarkMode from "@/components/01_propsBasic/DarkMode.vue";
+
+const message = ref('hello');
+const darkState = ref(false);
+
+function toggleDarkMode() {
+  darkState.value = !darkState.value;
+}
+</script>
+
+<template>
+  <div :class="{container: true, dark: darkState}">
+    props 입력: <input v-model="message"/>
+    <PropsChild :message="message"/>
+    <DarkMode @toggle="toggleDarkMode"/>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  border: 1px solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.dark {
+  background-color: black;
+  color: white;
+}
+</style>
