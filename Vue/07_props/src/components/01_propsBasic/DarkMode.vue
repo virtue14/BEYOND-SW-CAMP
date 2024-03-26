be1c644afd92eb04942f491fc58a31b16821825f
악덕 <rudejrqkr97@gmail.com>
2024-03-26 10:41:13 +0900

    docs(01_propsBasic): add comment

--- Vue/07_props/src/components/01_propsBasic/DarkMode.vue
+++ Vue/07_props/src/components/01_propsBasic/DarkMode.vue
@@ -4,7 +4,9 @@
   /* 부모로 부터 물려받은 toggle이라는 이름의 이벤트를 호출할 수 있는 함수 반환받기(defineEmits를 활용하면 배열로 이벤트 이름 작성) */
   const emit = defineEmits(['toggle']);
 </script>
-
+<!--
+    자식 컴포넌트가 부모 컴포넌트로 발산(emit(해당 이벤트 이름))
+-->
 <template>
   <button @click='emit("toggle")'>다크모드</button>
 </template>
