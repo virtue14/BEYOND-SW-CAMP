78dfefe7bbcc993717ea201fb5b4cc9274d65972
악덕 <rudejrqkr97@gmail.com>
2024-03-26 10:40:36 +0900

    feat(propsBasic): Add ReadProps.vue component
    - ReadProps.vue 컴포넌트 추가
    - defineProps를 사용하여 props 정의
    - readValue prop을 String으로 정의
    - props.readValue를 출력하는 h1 태그 추가
    - 자식의 readValue를 수정할 수 있는 input 태그 추가

---
+++ Vue/07_props/src/components/01_propsBasic/ReadProps.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {defineProps} from 'vue';
+const props = defineProps({
+  readValue: String
+});
+</script>
+
+<template>
+  <h1>{{props.readValue}}</h1>
+  자식의 readValue: <input v-model="props.readValue"/>
+</template>
+
+<style scoped>
+
+</style>
