100833edf498cc36765c2f52933df072b25305ad
악덕 <rudejrqkr97@gmail.com>
2024-03-26 09:46:53 +0900

    feat(components): add PropsParents component to App.vue
    - PropsParents 컴포넌트를 App.vue에 추가

--- Vue/07_props/src/App.vue
+++ Vue/07_props/src/App.vue
@@ -1,9 +1,16 @@
 <template>
+  <header>
+  <PropsParents/>
+  </header>
 
+  <main>
+
+  </main>
 </template>
 
 <script setup>
 
+import PropsParents from "@/components/01_propsBasic/PropsParents.vue";
 </script>
 
 <style scoped>
