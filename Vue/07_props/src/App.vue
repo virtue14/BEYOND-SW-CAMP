48ac3f3051a933fc03345ccd75c89bb252ed0c81
악덕 <rudejrqkr97@gmail.com>
2024-03-26 10:44:57 +0900

    feat(props): add drilling component for prop passing
    - 드릴링 컴포넌트 추가
    - PropsParents에서 DrillingParents로 변경
    - App.vue에 DrillingParents 추가
    - 02_propsDrilling/DrillingParents.vue 파일 추가

--- Vue/07_props/src/App.vue
+++ Vue/07_props/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <header>
-  <PropsParents/>
+    <PropsParents/>
   </header>
 
   <main>
-
+    <DrillingParents/>
   </main>
 </template>
 
 <script setup>
 
 import PropsParents from "@/components/01_propsBasic/PropsParents.vue";
+import DrillingParents from "@/components/02_propsDrilling/DrillingParents.vue";
 </script>
 
 <style scoped>
