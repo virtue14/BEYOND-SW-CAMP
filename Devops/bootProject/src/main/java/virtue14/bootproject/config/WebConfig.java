956c9c445314275defcf83135b4e32f08350fc9b
악덕 <rudejrqkr97@gmail.com>
2024-04-02 10:50:45 +0900

    feat(config): CORS 처리를 위한 WebConfig 클래스 추가
    - 'WebConfig.java' 파일에 CORS 처리를 위한 설정 클래스를 추가함.

---
+++ Devops/bootProject/src/main/java/virtue14/bootproject/config/WebConfig.java
@@ -0,0 +1,20 @@
+package virtue14.bootproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/* 설명: Rest-API 서버측에사 CORS 처리를 위한 설정 클래스 */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
