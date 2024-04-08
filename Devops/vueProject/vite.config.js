106582e65737604908f9074f5a1e1fe4cf1f53f4
악덕 <rudejrqkr97@gmail.com>
2024-04-08 11:28:10 +0900

    fix(vueProject): Vite 구성 파일에서 API 프록시 대상 변경
    - API 요청을 대상으로 하는 포트를 7777에서 8055로 변경함.

--- Devops/vueProject/vite.config.js
+++ Devops/vueProject/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server : {
     proxy: {
       '/api' : {
-        target: 'http://localhost:7777',
+        target: 'http://localhost:8055',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
