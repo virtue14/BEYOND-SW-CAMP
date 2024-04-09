cd4dd1b46b5aa74e15efcc9693d301bdf4a992a7
악덕 <rudejrqkr97@gmail.com>
2024-04-09 10:13:22 +0900

    feat(App.vue): 도커 컨테이너와 쿠버네티스 네트워크 설정
    - 백엔드를 도커 컨테이너로 8055포트로 만들었을 때
    - 도커 컨테이너 간에 네트워크 연결 후(5173/api가 아니라 8011/api로 바꿔야 한다.)
    - 백엔드를 쿠버네티스를 활용한 워커노트로 띄웠을 경우(30001번 NodePort)

--- Devops/vueProject/src/App.vue
+++ Devops/vueProject/src/App.vue
@@ -17,8 +17,15 @@
   const result = ref(0);
 
   const sendPlus = async() => {
+
+    /* 백엔드를 도커 컨테이너로 8055포트로 만들었을 때 */
     // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
-    const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+    /* 도커 컨테이너 간에 네트워크 연결 후(5173/api가 아니라 8011/api로 바꿔야 한다.) */
+    // const response = await fetch(`http://localhost:8011/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 백엔드를 쿠버네티스를 활용한 워커노트로 띄웠을 경우(30001번 NodePort) */
+    const response = await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);
+
     const data = await response.json();
     result.value = data.sum;
   }
